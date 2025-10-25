6fb2cf18ccac1d64b42fee9cbf3c2a5cb98dcdeb
NKY5223 <76890154+NKY5223@users.noreply.github.com>
2025-10-25 11:37:38 +0800

    Fix selection positioning

--- src/components/view/inspector/Inspector.tsx
+++ src/components/view/inspector/Inspector.tsx
@@ -1,9 +1,8 @@
 import { makeSection, makeSingle, makeSubmenu, useContextMenu } from "@components/contextmenu/ContextMenu.ts";
 import { useEditorSelection } from "@components/editor/selection.ts";
-import { DropdownSelect } from "@components/form/dropdown/DropdownSelect.tsx";
 import { FormSection } from "@components/form/FormSection.tsx";
 import { Icon } from "@components/icon/Icon.tsx";
-import { Layout, useLayoutTree } from "@components/layout/layout.ts";
+import { Layout } from "@components/layout/layout.ts";
 import { getObject, useDispatchSkapMap, useSkapMap } from "@editor/map.ts";
 import css from "./Inspector.module.css";
 import { ViewToolbar } from "@components/layout/LayoutViewToolbar.tsx";
@@ -13,113 +12,9 @@ import { BoundsInput } from "@editor/BoundsInput.tsx";
 import { Vec2Input } from "@editor/Vec2Input.tsx";
 import { FormTitle } from "@components/form/FormTitle.tsx";
 
-type Opts = Parameters<typeof DropdownSelect<number>>[0]["options"];
-
-const testOptions: Opts = [
-	{
-		name: "integer",
-		label: "Integers",
-		options: [
-			{
-				name: "zero",
-				value: 0,
-				label: "0",
-				icon: "counter_0",
-			},
-			{
-				name: "one",
-				value: 1,
-				label: "ONE",
-				// icon: "counter_1",
-			},
-			{
-				name: "two",
-				value: 2,
-				label: "twooooooooooooooooooooooooooo ooooooooooooooooooooooooooooo oooooooo",
-				icon: "counter_2",
-			},
-			{
-				name: "three",
-				value: 3,
-				label: "三",
-				icon: "counter_3",
-			},
-			{
-				name: "three.1",
-				value: 3,
-				label: "三",
-				icon: "counter_3",
-			},
-			{
-				name: "three.2",
-				value: 3,
-				label: "三",
-				icon: "counter_3",
-			},
-			{
-				name: "three.3",
-				value: 3,
-				label: "三",
-				icon: "counter_3",
-			},
-		]
-	},
-	{
-		name: "integer2",
-		label: "Integers 2",
-		icon: "numbers",
-		options: [
-			{
-				name: "zero",
-				value: 0,
-				label: "0",
-			},
-			{
-				name: "one",
-				value: 1,
-				label: "ONE",
-			},
-			{
-				name: "two",
-				value: 2,
-				label: "twooo",
-			},
-			{
-				name: "three",
-				value: 3,
-				label: "三",
-			},
-		]
-	},
-	{
-		name: "pi",
-		value: Math.PI,
-		label: "π",
-		icon: "circle",
-	},
-	{
-		name: "e",
-		value: Math.E,
-		label: "e",
-	},
-	{
-		name: "sqrt2",
-		value: Math.SQRT2,
-		label: "√2",
-		icon: "diagonal_line",
-	},
-	{
-		name: "masceroni",
-		value: 0.558,
-		label: "γ",
-		icon: "language_pinyin"
-	}
-];
-
 export const Inspector: Layout.ViewComponent = ({
 	viewSwitch,
 }) => {
-	const layout = useLayoutTree();
 	const selection = useEditorSelection();
 	const map = useSkapMap();
 	const dispatchMap = useDispatchSkapMap();
