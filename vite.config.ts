91ecd4a8cd1ce47cf35220d8619e5625a0ea849f
NKY5223 <76890154+NKY5223@users.noreply.github.com>
2025-10-27 15:28:26 +0800

    try fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,20 @@ import react from "@vitejs/plugin-react-swc";
 import path from "node:path";
 
 // https://vitejs.dev/config/
-export default defineConfig(env => ({
-	plugins: [react()],
-	base: env.mode === "PRODUCTION" ? "/SkapEdit/" : "/",
-	resolve: {
-		alias: {
-			"@common": path.join(__dirname, "./src/common"),
-			"@components": path.join(__dirname, "./src/components"),
-			"@hooks": path.join(__dirname, "./src/hooks"),
-			"@icon": path.join(__dirname, "./src/components/icon/icon"),
-			"@editor": path.join(__dirname, "./src/editor"),
+export default defineConfig(env => {
+	const base = env.mode === "PRODUCTION" ? "/SkapEdit/" : "/";
+	console.log("Base:", base);
+	return {
+		plugins: [react()],
+		base,
+		resolve: {
+			alias: {
+				"@common": path.join(__dirname, "./src/common"),
+				"@components": path.join(__dirname, "./src/components"),
+				"@hooks": path.join(__dirname, "./src/hooks"),
+				"@icon": path.join(__dirname, "./src/components/icon/icon"),
+				"@editor": path.join(__dirname, "./src/editor"),
+			}
 		}
-	}
-}));
+	};
+});
