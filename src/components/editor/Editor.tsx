811314f72e7e099a44da6eac9886e82ab2319096
NKY5223 <76890154+NKY5223@users.noreply.github.com>
2025-10-27 16:16:53 +0800

    Update README, fix warning

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -10,7 +10,7 @@ import { SelectionProvider } from "./selection.ts";
 import { LayoutRoot } from "@components/layout/Layout.tsx";
 import { TranslationProvider } from "@components/translate/TranslationProvider.tsx";
 import { translator_en_US } from "@components/translate/translation/en_US.ts";
-import { translator_zh_Hans } from "@components/translate/translation/zh_Hans.ts";
+// import { translator_zh_Hans } from "@components/translate/translation/zh_Hans.ts";
 
 type EditorProps = {
 
