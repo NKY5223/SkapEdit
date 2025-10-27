fc256886704a5915ee3fd1b93f80857ed1816c5f
NKY5223 <76890154+NKY5223@users.noreply.github.com>
2025-10-27 16:01:14 +0800

    i forgor

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
-	const env = loadEnv(mode, "env");
+	const env = loadEnv(mode, false);
 	console.log("env:", env);
 	console.log("GITHUB:", env.VITE_IS_GITHUB);
 
