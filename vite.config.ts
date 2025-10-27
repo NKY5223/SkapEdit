500b69b353b547b87ea3dab2893e21febaabe99d
NKY5223 <76890154+NKY5223@users.noreply.github.com>
2025-10-27 16:03:11 +0800

    Make vite config use ./ as the env location

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
-	const env = loadEnv(mode, false);
+	const env = loadEnv(mode, "./");
 	console.log("env:", env);
 	console.log("GITHUB:", env.VITE_IS_GITHUB);
 
